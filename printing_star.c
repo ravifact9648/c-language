fcc0e0151ee79a3e82192624ceadf1d6138d5411
Ravi_Prakash_Yadav <raviiprakashyadav.2020@gmail.com>
2021-01-01 16:57:24 +0530

    added printing star of 5 rows and 5 coloumns

---
+++ printing_star.c
@@ -0,0 +1,19 @@
+// print a sequence of 5 rows and 5 coloumn of star(*)
+// Ravi Prakash Yadav
+
+
+# include<stdio.h>
+
+int main(){
+for(int i=1;i<=5;i++)
+{
+    for(int j=1;j<=5;j++)
+    {
+        printf("*\t");
+
+    }
+    printf("\n\n");
+    // printf("\n");
+}
+return 0;
+}
